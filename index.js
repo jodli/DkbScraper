4e6e068df809cd0d9e17eaeb60acbb6ac28d35c7
jodli <job87@web.de>
2018-04-22 12:30:57 +0200

    Initial commit. Added start and login.

---
+++ index.js
@@ -0,0 +1,68 @@
+const puppeteer = require("puppeteer");
+const log = require("loglevel");
+
+require("dotenv").config();
+
+async function focusInputField(page, selector) {
+  log.info("Focusing input field");
+  const inputField = await page.$(selector);
+  await inputField.focus();
+  return inputField;
+}
+
+async function typeInInputField(inputField, text) {
+  log.info("Typing text", text);
+  await inputField.type(text, {
+    delay: 10
+  });
+}
+
+async function startAndNavigateToLoginPage(options) {
+  log.info("Setting up browser.");
+  const browser = await puppeteer.launch({
+    headless: !options.interactiveMode,
+    timeout: 60000,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1280,
+    height: 1280
+  });
+  await page.goto(process.env.BASE_URL, {
+    waitUntil: "domcontentloaded"
+  });
+
+  await page.waitFor(1000);
+  return { page, browser };
+}
+
+async function performLogin(page) {
+  log.info("Select name input field.");
+  const nameInputField = await focusInputField(
+    page,
+    process.env.LOGIN_NAME_INPUT
+  );
+  await typeInInputField(nameInputField, process.env.LOGIN_NAME);
+
+  log.info("Select pin input field.");
+  const pinInputField = await focusInputField(
+    page,
+    process.env.LOGIN_PIN_INPUT
+  );
+  await typeInInputField(pinInputField, process.env.LOGIN_PIN);
+
+  log.info("Pressing login button.");
+  await page.click(process.env.LOGIN_BUTTON);
+}
+
+(async () => {
+  log.setLevel(log.levels.TRACE);
+  const { page, browser } = await startAndNavigateToLoginPage({
+    interactiveMode: true
+  });
+  await performLogin(page);
+})().catch(error => {
+  log.error(error);
+});
