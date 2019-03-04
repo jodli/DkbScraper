5d9b5ed69eaa43c349464247984fae474650d7db
jodli <jobst87@gmail.com>
2019-03-04 15:20:30 +0100

    Code formatting

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const puppeteer = require("puppeteer");
-const log = require("loglevel");
-const path = require("path");
-const program = require("commander");
-const fs = require("fs-extra");
+const puppeteer = require('puppeteer');
+const log = require('loglevel');
+const path = require('path');
+const program = require('commander');
+const fs = require('fs-extra');
 
-require("dotenv").config();
+require('dotenv').config();
 
 // URLs
-const BaseUrl = "https://www.dkb.de/banking";
+const BaseUrl = 'https://www.dkb.de/banking';
 
 // Selectors
-const AjaxLoadingSpinner = "body > div.ajax_loading";
+const AjaxLoadingSpinner = 'body > div.ajax_loading';
 
-const LoginNameInput = "#loginInputSelector";
-const LoginPinInput = "#pinInputSelector";
-const LoginButton = "#buttonlogin";
-const LogoutButton = "#logout";
+const LoginNameInput = '#loginInputSelector';
+const LoginPinInput = '#pinInputSelector';
+const LoginButton = '#buttonlogin';
+const LogoutButton = '#logout';
 
-const TransactionsMenu = "#menu_0\\2e 0\\2e 0-node";
+const TransactionsMenu = '#menu_0\\2e 0\\2e 0-node';
 
 const AccountSelect = "select[id$='_slAllAccounts']";
 
@@ -25,7 +25,7 @@ const TransactionsDebitFrom = "input[id$='_transactionDate']";
 const TransactionsDebitTo = "input[id$='_toTransactionDate']";
 const TransactionsCreditFrom = "input[id$='_postingDate']";
 const TransactionsCreditTo = "input[id$='_toPostingDate']";
-const TransactionsSearchButton = "#searchbutton";
+const TransactionsSearchButton = '#searchbutton';
 
 const ExportCsvSelector =
   '#contentHome > div.clearfix.grid_4.col2 > div > h1 > a.noSpinner.evt-csvExport > span';
@@ -35,14 +35,14 @@ const TransactionType = Object.freeze({ debit: 0, credit: 1 });
 exports = module.exports = this;
 
 async function focusInputField(page, selector) {
-  log.debug("Focusing input field:", selector);
+  log.debug('Focusing input field:', selector);
   const inputField = await page.$(selector);
   return inputField;
 }
 
 async function clearText(page, selector) {
-  log.debug("Clearing input field:", selector);
-  await page.$eval(selector, input => (input.value = ""));
+  log.debug('Clearing input field:', selector);
+  await page.$eval(selector, input => (input.value = ''));
 }
 
 async function typeInInputField(inputField, text, isPassword = false) {
@@ -57,26 +57,26 @@ async function typeInInputField(inputField, text, isPassword = false) {
 }
 
 async function waitForNavigation(page) {
-  log.debug("Waiting for the navigation to complete.");
+  log.debug('Waiting for the navigation to complete.');
   await page.waitForNavigation({
-    waitUntil: "networkidle0"
+    waitUntil: 'networkidle0'
   });
 }
 
 async function waitForLoadingElements(page) {
-  log.debug("Waiting for loading elements to be removed.");
+  log.debug('Waiting for loading elements to be removed.');
   await page.waitForSelector(AjaxLoadingSpinner, {
     hidden: true
   });
 }
 
 async function createScreenshot(page, outFile) {
-  log.debug("Taking screenshot to file:", outFile);
+  log.debug('Taking screenshot to file:', outFile);
   await page.screenshot({ path: outFile });
 }
 
 async function startAndNavigateToLoginPage(options) {
-  log.info("Setting up browser.");
+  log.info('Setting up browser.');
   const browser = await puppeteer.launch({
     headless: !options.interactiveMode,
     timeout: 60000,
@@ -89,7 +89,7 @@ async function startAndNavigateToLoginPage(options) {
     height: 1280
   });
   await page.goto(BaseUrl, {
-    waitUntil: "domcontentloaded"
+    waitUntil: 'domcontentloaded'
   });
 
   await page.waitFor(1000);
@@ -97,42 +97,42 @@ async function startAndNavigateToLoginPage(options) {
 }
 
 async function performLogin(page) {
-  log.info("Performing Login.");
-  log.debug("Select name input field.");
+  log.info('Performing Login.');
+  log.debug('Select name input field.');
   const nameInputField = await focusInputField(page, LoginNameInput);
   await typeInInputField(nameInputField, process.env.LOGIN_NAME);
 
-  log.debug("Select pin input field.");
+  log.debug('Select pin input field.');
   const pinInputField = await focusInputField(page, LoginPinInput);
   await typeInInputField(pinInputField, process.env.LOGIN_PIN, true);
   await page.waitFor(250);
 
-  log.debug("Pressing login button.");
+  log.debug('Pressing login button.');
   await page.click(LoginButton);
 
   await waitForNavigation(page);
 }
 
 async function performLogout(page) {
-  log.info("Performing Logout.");
-  log.debug("Pressing logout button.");
+  log.info('Performing Logout.');
+  log.debug('Pressing logout button.');
   await page.click(LogoutButton);
 }
 
 async function navigateToTransactions(page) {
-  log.info("Navigating to Transaction page.");
+  log.info('Navigating to Transaction page.');
   page.click(TransactionsMenu);
   await waitForLoadingElements(page);
   await page.waitFor(1000);
 }
 
 async function getAllAccounts(page) {
-  log.debug("Waiting for account dropdown box to appear.");
+  log.debug('Waiting for account dropdown box to appear.');
   await page.waitForSelector(AccountSelect, {
     visible: true
   });
 
-  log.info("Getting all accounts from dropdown box.");
+  log.info('Getting all accounts from dropdown box.');
   const accounts = await page.evaluate(
     (selector, transactionType) => {
       const options = Array.from(document.querySelector(selector).options);
@@ -142,7 +142,7 @@ async function getAllAccounts(page) {
           name: option.label
         };
         account.type =
-          account.name.indexOf("Kreditkarte") !== -1
+          account.name.indexOf('Kreditkarte') !== -1
             ? transactionType.credit
             : transactionType.debit;
         return account;
@@ -157,21 +157,14 @@ async function getAllAccounts(page) {
 }
 
 async function selectAccount(page, account) {
-  log.info("Selecting account:", account.name);
+  log.info('Selecting account:', account.name);
   page.select(AccountSelect, account.id);
   await waitForLoadingElements(page);
   await page.waitFor(1000);
 }
 
 async function selectTimeRange(page, account, timeRange) {
-  log.info(
-    "Selecting time range for",
-    account.type,
-    ":",
-    timeRange.from,
-    "-",
-    timeRange.to
-  );
+  log.info('Selecting time range for', account.type, ':', timeRange.from, '-', timeRange.to);
 
   switch (account.type) {
     case TransactionType.credit:
@@ -190,10 +183,12 @@ async function selectTimeRange(page, account, timeRange) {
   const fromInputField = await focusInputField(page, fromInputSelector);
   await clearText(page, fromInputSelector);
   await typeInInputField(fromInputField, timeRange.from);
+  // await page.waitFor(250);
 
   const toInputField = await focusInputField(page, toInputSelector);
   await clearText(page, toInputSelector);
   await typeInInputField(toInputField, timeRange.to);
+  // await page.waitFor(250);
 
   page.click(TransactionsSearchButton);
   await waitForLoadingElements(page);
@@ -237,19 +232,16 @@ class DkbScraper {
   async scrape(accounts) {
     const { page, browser } = await startAndNavigateToLoginPage({
       interactiveMode: this.options.interactiveMode,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
-    await createScreenshot(
-      page,
-      path.join(this.options.screenshotDir, "before_login.png")
-    );
+    await createScreenshot(page, path.join(this.options.screenshotDir, 'before_login.png'));
     await performLogin(page);
 
     try {
       await navigateToTransactions(page);
     } catch (error) {
-      await handleError(page, "navigateToTransactions:", error);
+      await handleError(page, 'navigateToTransactions:', error);
     }
 
     const timeRange = { from: this.options.from, to: this.options.to };
@@ -257,17 +249,15 @@ class DkbScraper {
     try {
       allAccounts = await getAllAccounts(page);
     } catch (error) {
-      await handleError(page, "getAllAccounts:", error);
+      await handleError(page, 'getAllAccounts:', error);
     }
 
     const accountsToScrape =
-      accounts[0] === "all"
+      accounts[0] === 'all'
         ? allAccounts
         : allAccounts.filter(
             allAccount =>
-              accounts.filter(
-                account => allAccount.name.indexOf(account) !== -1
-              ).length > 0
+              accounts.filter(account => allAccount.name.indexOf(account) !== -1).length > 0
           );
 
     for (let index = 0; index < accountsToScrape.length; index++) {
@@ -280,18 +270,15 @@ class DkbScraper {
 
         await getCsvExport(page, this.options.outputFolder);
       } catch (error) {
-        await handleError(page, "getTransactionsForAccount:", error);
+        await handleError(page, 'getTransactionsForAccount:', error);
       }
     }
 
     await performLogout(page);
-    await createScreenshot(
-      page,
-      path.join(this.options.screenshotDir, "after_logout.png")
-    );
+    await createScreenshot(page, path.join(this.options.screenshotDir, 'after_logout.png'));
 
     browser.close();
-    log.info("Closed browser.");
+    log.info('Closed browser.');
     process.exit(0);
   }
 }
@@ -300,50 +287,45 @@ log.setLevel('warn');
 program.version('0.2.0');
 
 program
-  .command("scrape [accounts...]")
+  .command('scrape [accounts...]')
   .description(
     "Which account(s)? Either specify an IBAN, a credit card number or type 'all' to query all accounts."
   )
-  .option("--from <from>", "From which date?")
-  .option("--to <to>", "Until which date?")
-  .option("-v, --verbose", "Enables verbose logging.")
-  .option("-t, --trace", "Enables trace logging.")
-  .option(
-    "-s, --screenshotDir <screenshotDir>",
-    "Specifies visual logging via screenshots."
-  )
-  .option(
-    "-o, --outputFolder <outputFolder>",
-    "Specifies output folder for transactions."
-  )
-  .option("-i, --interactive-mode", "Shows the browser window.")
+  .option('--from <from>', 'From which date?')
+  .option('--to <to>', 'Until which date?')
+  .option('-v, --verbose', 'Enables verbose logging.')
+  .option('-t, --trace', 'Enables trace logging.')
+  .option('-s, --screenshotDir <screenshotDir>', 'Specifies visual logging via screenshots.')
+  .option('-o, --outputFolder <outputFolder>', 'Specifies output folder for transactions.')
+  .option('-i, --interactive-mode', 'Shows the browser window.')
   .action(async (accounts, options) => {
     if (accounts.length === 0 || !options.from || !options.to) {
       program.help();
     }
     if (options.verbose) {
-      log.setLevel("info");
-      log.info("Enabled verbose logging.");
+      log.setLevel('info');
+      log.info('Enabled verbose logging.');
     }
     if (options.trace) {
       log.enableAll();
-      log.trace("Enabled trace logging.");
+      log.trace('Enabled trace logging.');
     }
     options.interactiveMode = options.interactiveMode || false;
     if (options.interactiveMode) {
-      log.info("Running the browser in interactive mode.");
+      log.info('Running the browser in interactive mode.');
     }
 
-    options.screenshotDir = options.screenshotDir || "./screenshots";
-    log.info("Enabled screenshots to: " + options.screenshotDir);
+    options.screenshotDir = options.screenshotDir || './screenshots';
+    log.info('Enabled screenshots to: ' + options.screenshotDir);
     fs.ensureDirSync(options.screenshotDir);
 
     options.outputFolder = options.outputFolder || './exports';
+    log.info('Enabled exports to: ' + options.outputFolder);
 
-    log.info("Scraping transactions for accounts:", accounts);
+    log.info('Scraping transactions for accounts:', accounts);
     const scraper = new DkbScraper(options);
     await scraper.scrape(accounts).catch(error => {
-      log.error("globalScope:", error);
+      log.error('globalScope:', error);
       process.exit(1);
     });
   });
